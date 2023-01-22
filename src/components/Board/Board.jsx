d73e90a848c8da1e2ceb03ccda1f7d270def7e61
Connor <connordanielfrank@gmail.com>
2023-01-22 17:28:26 +1300

    dev: Update Board

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -3,10 +3,18 @@ import Card from '../Card/Card';
 import styles from './Board.module.css';
 
 export default function Board() {
+  const cards = [];
+
+  // State?
+  for (let i = 0; i < 12; i++) {
+    cards.push(
+      <Card image={i} />,
+    );
+  }
+
   return (
     <div className={styles.board}>
-      <Card image={1} />
-      <Card image={2} />
+      {cards}
     </div>
   );
 }
